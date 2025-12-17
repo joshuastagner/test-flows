78ae689518a3996b61bdc57827c6a6ec90016fd1
Josh Stagner <josh.stagner@prefect.io>
2025-12-17 11:31:07 -0800

    Fire and forget tasks

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
 from time import sleep
 
-from prefect import flow
+from prefect import flow, task
+from prefect.logging import get_run_logger
+
+
+@task
+def sleep_task(i: str):
+    logger = get_run_logger()
+    logger.info(f"starting task {i}")
+    sleep(300)
+    logger.info(f"finished task {i}")
 
 
 @flow
 def sleep_awhile():
-    print("Gonna sleep while you break stuff....")
-    sleep(300)
+    logger = get_run_logger()
+    logger.info("starting some tasks")
+    for i in range(5):
+        sleep_task.submit(i)
+    logger.info("Gonna sleep while you break stuff....")
+    sleep(120)
+    
